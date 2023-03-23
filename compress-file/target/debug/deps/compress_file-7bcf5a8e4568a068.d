6796ee19116ee8581d82157a024694f5bd287a5e
xeejay <tiwaridhananjay70@gmail.com>
2023-03-23 16:45:19 +0530

    cargo.toml updated

---
+++ compress-file/target/debug/deps/compress_file-7bcf5a8e4568a068.d
@@ -0,0 +1,5 @@
+/home/djay/Desktop/compress-file/compress-file/target/debug/deps/compress_file-7bcf5a8e4568a068.rmeta: src/main.rs
+
+/home/djay/Desktop/compress-file/compress-file/target/debug/deps/compress_file-7bcf5a8e4568a068.d: src/main.rs
+
+src/main.rs:
---
+++ compress-file/target/debug/deps/compress_file-817e287e254beb22.d
@@ -0,0 +1,5 @@
+/home/djay/Desktop/compress-file/compress-file/target/debug/deps/compress_file-817e287e254beb22.rmeta: src/main.rs
+
+/home/djay/Desktop/compress-file/compress-file/target/debug/deps/compress_file-817e287e254beb22.d: src/main.rs
+
+src/main.rs:
